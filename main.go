ff0a52fad40b2b298454b99f591c154f27699772
Sun <95302870@qq.com>
2023-03-05 20:15:13 +0800

    更新介绍文件及版本号

--- main.go
+++ main.go
@@ -59,6 +59,7 @@ func main() {
 	if err := initialize.ConnectDb(); err != nil {
 		// global.Logger.Errorln("failed to init db, err:%+v", err)
 		global.Logger.Errorln("数据库连接错误", err.Error())
+		os.Exit(1)
 	}
 
 	// 用户不存在创建用户
