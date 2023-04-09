800e9bbf1e5ab9f05a1f57c3d60ac587187c2349
Sun <95302870@qq.com>
2023-04-09 22:41:42 +0800

    优化 windows编译脚本

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 cd web 
-npm run build 
+call npm run build 
 rd ..\assets\frontend
 xcopy .\dist\ ..\assets\frontend /s /i /y
 cd ..\
 go-bindata-assetfs -o=assets/bindata.go -pkg=assets assets/...
 go build -o li-calendar.exe --ldflags="-X main.RunMode=release" main.go
+echo "li-calendar.exe compilation is complete!"
